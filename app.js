0d3d9be609d1279fa899e9a24582d1ad973978c0
abrahamMartinez <bmtzs@me.com>
2023-08-09 18:18:52 +0200

    Hover buttons

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+const colors = ['red', 'blue', 'yellow', 'green'];
+
+
 const container = document.querySelector('button');
 container.addEventListener('click', () => {
     // changeDisplay
     const color = getRandomColor();
     document.body.style.backgroundColor = color;   
-    const h1 = document.querySelector('h1')
-    h1.innerHTML = getRandomLimb() + '\n' + color;
+    const h1 = document.querySelector('h1');
+    h1.innerHTML = getRandomLimb() + '\n' + (color.charAt(0).toUpperCase() + color.slice(1));
 });
 
 function getRandomLimb () {
@@ -17,3 +20,7 @@ function getRandomColor() {
     return color[Math.floor(Math.random() * color.length)];
 }
 
+const addColorBtn = document.getElementById('add-colors-btn')
+addColorBtn.addEventListener('click', () => {
+    console.log("Add color button");
+})
