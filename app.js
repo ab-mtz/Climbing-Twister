21a1320affbd9825fe9ec45cef5bb61abb33de14
abrahamMartinez <bmtzs@me.com>
2023-08-09 18:56:45 +0200

    Interval working and hiding add colors btn

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const colors = ['red', 'blue', 'yellow', 'green'];
 
+let interval = null;
+
+const setIntervalBtn = document.getElementById('set-interval-btn');
+let intervalActive = false;
+let intervalId; // Store the interval ID
 
 const container = document.querySelector('button');
 container.addEventListener('click', () => {
-    // changeDisplay
+    if(!intervalActive){
+        generateMove();
+    }
+});
+
+function generateMove() {
     const color = getRandomColor();
     document.body.style.backgroundColor = color;   
     const h1 = document.querySelector('h1');
     h1.innerHTML = getRandomLimb() + '\n' + (color.charAt(0).toUpperCase() + color.slice(1));
-});
+}
 
 function getRandomLimb () {
     const limb = ["Left Arm", "Right Arm", "Left Leg", "Right Leg"];
@@ -24,3 +34,26 @@ const addColorBtn = document.getElementById('add-colors-btn')
 addColorBtn.addEventListener('click', () => {
     console.log("Add color button");
 })
+
+
+
+setIntervalBtn.addEventListener('click', () => {
+    if (!intervalActive) {
+        console.log("Interval Activated");
+        intervalId = setInterval(() => {
+            generateMove();
+        }, 4000); // Corrected the placement of the interval duration
+        setIntervalBtn.innerHTML = "Stop"
+        intervalActive = true;
+        addColorBtn.style.visibility='collapse';
+    } else {
+        console.log("Interval Deactivated");
+        clearInterval(intervalId); // Clear the interval using the stored ID
+        intervalActive = false;
+        const h1 = document.querySelector('h1');
+        h1.innerHTML = 'Click to Start';
+        setIntervalBtn.innerHTML = "Automatic (4s)";
+        addColorBtn.style.visibility='visible';
+    }
+});
+
